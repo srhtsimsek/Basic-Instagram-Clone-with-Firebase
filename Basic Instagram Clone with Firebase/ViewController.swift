8534f6d834628a0bd710c0f6719b6f1957029a73
srhtsimsek <srhtsimsekk@gmail.com>
2023-06-07 16:01:03 +0300

    Initial Commit

---
+++ Basic Instagram Clone with Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Instagram Clone with Firebase
+//
+//  Created by Serhat  Şimşek  on 7.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
